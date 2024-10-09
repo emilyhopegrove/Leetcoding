48408356987ab2d0e5ca184bcfbc29a33f5e8763
Emily Grove <109101666+emilyhopegrove@users.noreply.github.com>
2024-10-08 19:49:16 -0700

    Time: 795 ms (37.75%) | Memory: 20.8 MB (65.75%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,29 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        #input: array of numbers, each number is a price and it's position is the day
-        #output: Max difference
-
-        #Fast/slow pointer
-        #Must loop through the array
-        #Must start at first element, set up one pointer that is slow
-        #set up a second pointer that starts at the second element, and moves forward one at a time each loop to check the difference between second pointer and first pointer. 
-        #move the fast pointer each iteration
-        #return the max difference
-
+        # Initialize min price as infinity and max profit as 0
+        min_price = float('inf')  # Use float('inf') for Infinity
         max_profit = 0
 
-        l, r = 0, 1
-
-        while r < len(prices):
-            #if the profit makes sense, ie if it's not negative
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
-
-            #otherwise if left price is greater than right price, move pointers accordingly. right moves forward one and left moves to where right was
-            else:
-                l = r
-            r += 1
+        # Iterate over prices
+        for price in prices:
+            # Update min_price to be the lowest encountered price
+            min_price = min(min_price, price)
+            # Calculate the profit if selling at the current price
+            profit = price - min_price
+            # Update max_profit if this profit is higher
+            max_profit = max(max_profit, profit)
 
         return max_profit
